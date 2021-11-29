f3027a856b09c6a140befa4c9eb5bc93344f74ee
Андрей М <andreimurysev@yandex.ru>
2021-11-29 16:57:04 +0300

    deploy33

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = env('SECRET_KEY', default=None)
 
 DEBUG = False
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default='["*"]')
 
 AUTH_USER_MODEL = 'users.User'
 
